8e1b21485d59d2ff016b4399973dbb0f28b9648c
Sergey <llan@yandex.ru>
2023-06-04 20:15:33 +0300

    edit

--- Lesson9.py
+++ Lesson9.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Задача 40: Работать с файлом california_housing_train.csv, который находится в папке sample_data. 
 #Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population).
 import pandas as pd
@@ -6,13 +5,4 @@ df = pd.read_csv('sample_data/california_housing_train.csv')
 df[df['population']<501]['median_house_value'].agg(['mean'])
 
 #Задача 42: Узнать какая максимальная households в зоне минимального значения population.
-=======
-#Задача 40: Работать с файлом california_housing_train.csv, который находится в папке sample_data. 
-#Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population).
-import pandas as pd
-df = pd.read_csv('sample_data/california_housing_train.csv')
-df[df['population']<501]['median_house_value'].agg(['mean'])
-
-#Задача 42: Узнать какая максимальная households в зоне минимального значения population.
->>>>>>> 313374779771dc1f21e6da19ba94ebeba7584db2
 df[df['population']==df['population'].min()]['households'].max()
